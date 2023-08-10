6124d6b7f5e438f53e2e52becb95a00806ab0d69
Harsh Kumar <harsh@noreply.com>
2023-08-10 14:42:23 +0530

    Thu Aug 10 14:42:23 IST 2023

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 echo "starting backup........."
-git add ~/.config/nvim
+git -C ~/.config add ~/.config/nvim
+git -C ~/.config add -u 
+git -C ~/.config commit -m "$(date)"
+git -C ~/.config push
+git -C ~/notes add -A
 git commit -m "$(date)"
-git push
 echo "backup done"
