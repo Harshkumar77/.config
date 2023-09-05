9aadebac7de35b12c47eabf7b056e66179c667ec
Harsh Kumar <harsh@noreply.com>
2023-09-05 19:58:14 +0530

    4989d257f3983940e73b04c1a023b7d0

--- nvim/lua/volcano/vimwiki.lua
+++ nvim/lua/volcano/vimwiki.lua
@@ -1,11 +1,12 @@
 vim.opt.dictionary:append('/usr/share/dict/words')
 
-vim.g.vimwiki_list = { {
-  path = '~/notes/',
-  syntax = 'markdown',
-  ext = '.md'
-} }
-
-vim.g.vimwiki_markdown_link_ext = 1
-
-vim.g.vimwiki_diary_caption_level=-1
+-- Not working
+-- vim.g.vimwiki_list = { {
+--   path = '~/notes/',
+--   syntax = 'markdown',
+--   ext = '.md'
+-- } }
+--
+-- vim.g.vimwiki_markdown_link_ext = 1
+--
+-- vim.g.vimwiki_diary_caption_level=-1
