407729c0eae8f1f9c4d75af79a302fec0069dad8
volcano <volcano@example.com>
2024-04-03 00:43:57 +0530

    7ac41549-e927-4ee3-a1ad-159402d17158

--- backup.sh
+++ backup.sh
@@ -1,28 +1,40 @@
 msg="$(uuidgen)"
+backup () {
+  git -C ~/.config add ~/.config/nvim
+  git -C ~/.config add ~/.config/wallpaper
+  git -C ~/.config add ~/.config/home-dot-files
+  git -C ~/.config add ~/.config/backup.sh
+  git -C ~/.config add ~/.config/bin 
+  git -C ~/.config add ~/.config/go-sys-scipts
+  git -C ~/.config add ~/.config/cronjob
+  git -C ~/.config add -u 
+  git -C ~/.config commit -m "$msg"
+  git -C ~/.config push
+
+  git -C ~/notes pull
+  git -C ~/notes add -A
+  git -C ~/notes commit -m "$msg"
+  git -C ~/notes push
+
+  echo '```' > ~/notes/Tasklist.md
+  task next >> ~/notes/Tasklist.md
+  echo '```' >> ~/notes/Tasklist.md
+  echo '```' >> ~/notes/Tasklist.md
+  timew day >> ~/notes/Tasklist.md
+  echo '```' >> ~/notes/Tasklist.md
+}
+
+errorWindow ()
+{
+  
+if [ ! -z "$WSLENV" ] 
+then
+  alacritty.exe --hold -e "wsl.exe echo Error while backup"
+else
+  alacritty --hold -e "echo Error while backup"
+fi
+}
 
 echo "starting backup........."
-
-git -C ~/.config add ~/.config/nvim
-git -C ~/.config add ~/.config/wallpaper
-git -C ~/.config add ~/.config/home-dot-files
-git -C ~/.config add ~/.config/backup.sh
-git -C ~/.config add ~/.config/bin 
-git -C ~/.config add ~/.config/go-sys-scipts
-git -C ~/.config add ~/.config/cronjob
-git -C ~/.config add -u 
-git -C ~/.config commit -m "$msg"
-git -C ~/.config push
-
-git -C ~/notes pull
-git -C ~/notes add -A
-git -C ~/notes commit -m "$msg"
-git -C ~/notes push
-
-echo '```' > ~/notes/Tasklist.md
-task next >> ~/notes/Tasklist.md
-echo '```' >> ~/notes/Tasklist.md
-echo '```' >> ~/notes/Tasklist.md
-timew day >> ~/notes/Tasklist.md
-echo '```' >> ~/notes/Tasklist.md
-
+backup || errorWindow
 echo "backup done"
