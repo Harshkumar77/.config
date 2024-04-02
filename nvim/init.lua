ac2d5b8b373a6d62c4852b3590dda4fbaadfb19c
volcano <volcano@example.com>
2024-04-02 17:00:01 +0530

    8c666ad5-956b-4eeb-8da1-ffff98dd8ade

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,4 +166,47 @@ require 'volcano.autocmd'
 require 'volcano.vimwiki'
 require 'volcano.last_nth_bfr'
 
+-- TODO
+-- local util = require "formatter.util"
+--
+-- require("formatter").setup {
+--   logging = true,
+--   log_level = vim.log.levels.WARN,
+--   filetype = {
+--     -- Formatter configurations for filetype "lua" go here
+--     -- and will be executed in order
+--     lua = {
+--       -- "formatter.filetypes.lua" defines default configurations for the
+--       -- "lua" filetype
+--       require("formatter.filetypes.lua").stylua,
+--
+--       -- You can also define your own configuration
+--       function()
+--         -- Supports conditional formatting
+--         if util.get_current_buffer_file_name() == "special.lua" then
+--           return nil
+--         end
+--
+--         -- Full specification of configurations is down below and in Vim help
+--         -- files
+--         return {
+--           exe = "stylua",
+--           args = {
+--             "--search-parent-directories",
+--             "--stdin-filepath",
+--             util.escape_path(util.get_current_buffer_file_path()),
+--             "--",
+--             "-",
+--           },
+--           stdin = true,
+--         }
+--       end
+--     },
+--
+--     ["*"] = {
+--       require("formatter.filetypes.any").remove_trailing_whitespace
+--     }
+--   }
+-- }
+
 vim.cmd [[ source ~/.config/nvim/nvimrc ]]
