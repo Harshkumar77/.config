3bc1e5189ed6c8860487a50c2a986bef1d95d604
volcano <volcano@example.com>
2024-04-03 19:15:01 +0530

    13795a56-ed06-4354-9d9b-e7ea077c2651

--- nvim/lua/volcano/theme.lua
+++ nvim/lua/volcano/theme.lua
@@ -1,26 +1,26 @@
 local theme = 'monokai_soda'
-require 'synthwave84'.setup({
-  glow = {
-    error_msg = true,
-    type2 = true,
-    func = true,
-    keyword = true,
-    operator = false,
-    buffer_current_target = true,
-    buffer_visible_target = true,
-    buffer_inactive_target = true,
-  }
-})
+-- require 'synthwave84'.setup({
+--   glow = {
+--     error_msg = true,
+--     type2 = true,
+--     func = true,
+--     keyword = true,
+--     operator = false,
+--     buffer_current_target = true,
+--     buffer_visible_target = true,
+--     buffer_inactive_target = true,
+--   }
+-- })
+--
+-- require 'onedark'.setup {
+--   style = 'deep'
+-- }
+--
 
-require 'onedark'.setup {
-  style = 'deep'
-}
-
-
-require('monokai').setup {}
-require('monokai').setup { palette = require('monokai').pro }
+-- require('monokai').setup {}
+-- require('monokai').setup { palette = require('monokai').pro }
 require('monokai').setup { palette = require('monokai').soda }
-require('monokai').setup { palette = require('monokai').ristretto }
+-- require('monokai').setup { palette = require('monokai').ristretto }
 
 
 vim.cmd('colorscheme ' .. theme)
