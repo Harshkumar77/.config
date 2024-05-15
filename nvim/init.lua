9af85a17f9b240e4a1631ec537023a5647b22b59
volcano <volcano@example.com>
2024-05-15 12:45:01 +0530

    b9933765-38d9-4569-8aa1-874fa066873a

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
+    'tpope/vim-fugitive',
 
     {
         -- LSP Configuration & Plugins
