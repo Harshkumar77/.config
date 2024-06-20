15433102ef045b5ae4ca856d27ea0d55251eec49
volcano <volcano@example.com>
2024-06-21 01:45:01 +0530

    aecde73f-0a0a-4f9a-aabb-a0fc50b166f7

--- go-sys-scipts/age.go
+++ go-sys-scipts/age.go
@@ -4,16 +4,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	FORMAT := "2006-12-14"
+	now := time.Now()
+	FORMAT := "2006-01-02"
 	dates := []string{
 		"2024-06-19",
 	}
-	now := time.Now()
+	output := []string{}
+	for i := 0; i < len(dates); i++ {
+		parsedDate, err := time.Parse(FORMAT, dates[i])
+		if err != nil {
+			log.Panic(err)
+		}
+		output = append(output, fmt.Sprintf("%.0f", parsedDate.Sub(now).Hours()/24))
+	}
 	age := now.Local().Year() - 2003 - 1
 
 	nextBdayYear := now.Year()
@@ -33,11 +42,11 @@ func main() {
 
 	daysTillNextYear := 365 - now.YearDay()
 
-	output := []string{
-		fmt.Sprint(daysTillNextYear),
-		daysTillNextBday,
-		fmt.Sprint(age),
-	}
+	output = append(output, fmt.Sprint(daysTillNextYear))
+	output = append(output, daysTillNextBday)
+	output = append(output, fmt.Sprint(age))
+
+	outputFormat := ""
 
 	for i := len(output) - 1; i > 0; i-- {
 		swapIdx := rand.Intn(i)
@@ -50,7 +59,10 @@ func main() {
 		temp := output[swapIdx]
 		output[swapIdx] = output[i]
 		output[i] = temp
+		outputFormat += "%v "
 	}
+
+	print(outputFormat)
 
 	fmt.Printf("%v %v %v\n", output[0], output[1], output[2])
 }
