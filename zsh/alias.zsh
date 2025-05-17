3f74dc3559571d7b0786bb756a957c0f9458a013
V_V <v@v.___>
2025-05-17 03:06:16 +0000

    ea15aae9-70d5-4f63-ac24-1bcc4217b068

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -11,15 +11,26 @@ alias gc="git commit"
 alias ga="git add"
 alias gpl='git add .; git commit -m $(uuidgen); git push'
 
-alias dmenu='/mnt/c/Program\ Files/wlines/wlines.exe | sed 's/.$//''
+bookmarked_folders="~/.config
+~"
 
-alias man='batman'
+if ! type "$(wsl.exe --version)" > /dev/null; then
+  # echo wsl
+  alias dmenu='/mnt/c/Program\ Files/wlines/wlines.exe | sed 's/.$//''
 
-# Browser
-alias firefox="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
-win_username="$(cmd.exe /c echo %username% |  tr -d '\r' )"
+  # Browser
+  alias firefox="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
 
-bookmarked_folders="/mnt/c/Users/$win_username/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup
+  win_username="$(cmd.exe /c echo %username% |  tr -d '\r' )"
+  
+  bookmarked_folders="${bookmarked_folders}
+/mnt/c/Users/$win_username/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup
 /mnt/c/notes/
 /mnt/c/Users/$win_username/Downloads/
 /mnt/c/Users/$win_username/Videos/"
+fi
+
+
+alias man='batman'
+
+
