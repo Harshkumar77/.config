98155e95b5b14af9a594c0d9625f9bf07c3f84a5
Your Name <you@example.com>
2025-12-01 09:55:17 +0530

    backup Monday 01 December 2025 09:55:17 AM IST

--- custom.zsh
+++ custom.zsh
@@ -1,3 +1,5 @@
+export EDITOR='vim'
+
 eval "$(zoxide init zsh)"   # For Zsh
 alias cd='z'
 alias l='fd --hidden --max-depth 1'
@@ -22,4 +24,20 @@ rmf() {
 	done
 }
 
-alias extract_urls='xidel -e "//a/@href'
+alias awesome-launch="killall xfwm4 && awesome"
+alias xfce-launch="killall awesome && xfwm4"
+
+
+vlc-random(){
+	OUT="/tmp/vlc-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8).m3u"
+	fd . -e mkv -a | shuf > "$OUT"
+	nohup vlc "$OUT" >/dev/null 2>&1 &
+}
+
+stream-mpv(){
+	url=$(zenity --entry --title="url")
+	url=$(yt-dlp --get-url "$(url)") 
+	nohup mpv "$(url)" 
+}
+alias extract-urls='xidel -e "//a/@href'
+alias copy-file='xclip -selection clipboard < '
