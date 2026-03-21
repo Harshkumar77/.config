724011dda1d853689cd0a894d218bc217a2321ff
Your Name <you@example.com>
2026-03-22 01:08:47 +0530

    backup Sunday 22 March 2026 01:08:47 AM IST

--- awesome/awesome-current-layout.sh
+++ awesome/awesome-current-layout.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-awesome-client 'return mouse.screen.selected_tag.layout.name' | awk -F'"' '{print $2}'
+awesome-client 'return mouse.screen.selected_tag.layout.name' | awk -F'"' '{printf $2}'
+
+printf " " 
+printf 3
+
