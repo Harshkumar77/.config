72314e2025c79b6dd2d541e6ba1f7a7af79c2ca8
Your Name <you@example.com>
2026-03-27 10:52:57 +0530

    backup Friday 27 March 2026 10:52:57 AM IST

---
+++ awesome/rofi-general.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rofi -title '' -modes combi -show combi -combi-modes window,layout:~/.config/awesome/rofi-selector2.sh,power-menu:rofi-power-menu,drun,folders:~/.config/awesome/pdf-selector.sh,run
+
+
