73598fdb9caee29cfa49918067c06fe7b25b5745
Your Name <you@example.com>
2026-03-27 13:29:16 +0530

    backup Friday 27 March 2026 01:29:16 PM IST

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -9,6 +9,7 @@ local awful = require("awful")
 awful.spawn.with_shell("picom")
 awful.spawn("redshift -x")
 awful.spawn("redshift -O 3500")
+awful.spawn("redshift -O 3500")
 awful.spawn.with_shell("~/.config/scripts/backup.sh")
 -- awful.spawn.with_shell("~/.config/awesome/Long.sh")
 
