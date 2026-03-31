1b744793a5fd573f7f05482cfa9b6a9dbdbfe3b1
Your Name <you@example.com>
2026-03-31 21:38:45 +0530

    backup Tuesday 31 March 2026 09:38:45 PM IST

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "bashls" }
+local servers = { "html", "cssls", "ts_ls", "bashls" , "svelte"}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
