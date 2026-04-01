7c2c8c356df06bf6c6d80f3e3446624b3e2e70d5
Your Name <you@example.com>
2026-04-01 13:32:40 +0530

    backup Wednesday 01 April 2026 01:32:40 PM IST

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,72 +1,45 @@
 #!/bin/bash
-
-backupTimeRaw='~/.config/tmp/config-backup-timestamp.txt'
-backupTime=$(cat "$backupTimeRaw")
-echo $backupTime
-
-hasOneHourPassed=`node -e "
-            const diff = (
-              ( Number(new Date()) - Number(new Date('$backupTime')) ) / (60*60*1000)
-            )
-
-            console.log(
-                (Math.random() < 0.25 || diff > 1) ? true : false
-              )
-            "`
-if [[ "$hasOneHourPassed" = "false" ]]; then
-    awesome-client '
-       local naughty = require("naughty")
-        naughty.notify({
-                text = "config: skipping backup",
-        })'
-    # notify-send "config: skipping backup"
-    exit
-fi
-
 cd ~/.config
 
-yay -Qmq > ~/.config/pkgs/aur.txt
-yay -Qnq > ~/.config/pkgs/repo.txt
+shouldRegularBackup=`node -p "(Math.random() < 0.40) ? true : false"`
 
-git add wallpaper
-git add nvim
+if [[ "$shouldRegularBackup" = "true" ]]; then
+    yay -Qmq > ~/.config/pkgs/aur.txt
+    yay -Qnq > ~/.config/pkgs/repo.txt
 
-git add -u
-git commit -m "backup $(date)"
+    git add wallpaper
+    git add nvim
 
-git push
+    git add -u
+    git commit -m "backup $(date)"
 
-awesome-client '
+    git push
+
+    awesome-client '
      local naughty = require("naughty")
       naughty.notify({
               text = "config: backup done",
       })'
-# notify-send "config: backup done"
-
-node -p 'new Date()' > "$backupTimeRaw"
+    # notify-send "config: backup done"
+fi
 
 
 # monthly
-monthly_backupTimeRaw='~/.config/tmp/monthly-config-backup-timestamp.txt'
-backupTime=$(cat "$monthly_backupTimeRaw")
-echo $backupTime
-
-hasOneMonthPassed=`node -e "
-            const diff = (
-              ( Number(new Date()) - Number(new Date('$backupTime')) ) / (60*60*1000*24*30)
-            )
-
-            console.log(
-                (Math.random() < 0.25 || diff > 1) ? true : false
-              )
+shouldRareBackup=`node -p "
+                (Math.random() < 0.10) ? true : false
             "`
-if [[ "$hasOneMonthPassed" = "true" ]]; then
+if [[ "$shouldRareBackup" = "true" ]]; then
     awesome-client '
        local naughty = require("naughty")
         naughty.notify({
                 text = "config: monthly backup completed",
         })'
     # notify-send "config: skipping backup"
-    node -p 'new Date()' > "$monthly_backupTimeRaw"
+else
+    awesome-client '
+       local naughty = require("naughty")
+        naughty.notify({
+                text = "config: skipping monthly backup",
+        })'
+    # notify-send "config: skipping backup"
 fi
-
