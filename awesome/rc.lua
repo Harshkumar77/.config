5f61822ab643a67e1bd13e220dcb3425999b154c
Your Name <you@example.com>
2026-04-01 10:09:26 +0530

    backup Wednesday 01 April 2026 10:09:26 AM IST

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -647,9 +647,9 @@ awful.rules.rules = {
 	{ rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = false } },
 
 	-- Set Firefox to always map on the tag named "2" on screen 1.
-	{ rule = { class = "Firefox Beta" }, properties = { screen = 1, tag = "browser" } },
+	-- { rule = { class = "Firefox Beta" }, properties = { screen = 1, tag = "browser" } },
 	{ rule = { class = "mpv" }, properties = { screen = 1, tag = "todo" } },
-	{ rule = { class = "alacritty" }, properties = { screen = 1, tag = "2" } },
+	-- { rule = { class = "alacritty" }, properties = { screen = 1, tag = "2" } },
 }
 -- }}}
 
