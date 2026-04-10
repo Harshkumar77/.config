f0a38325b5f0d8d8e8e24195440fd49824ec2020
Your Name <you@example.com>
2026-04-11 03:00:01 +0530

    backup Saturday 11 April 2026 03:00:01 AM IST

--- awesome/rofi-general.sh
+++ awesome/rofi-general.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rofi -title '' -modes combi -show combi -combi-modes window,layout:~/.config/awesome/rofi-selector2.sh,power-menu:rofi-power-menu,drun,run
+rofi -p "jkkk" -modes combi -show combi -combi-modes window,layout:~/.config/awesome/rofi-selector2.sh,power-menu:rofi-power-menu,drun,run
 
