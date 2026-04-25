ab3648ef239c56b8fc09be9133d9c8e164fb0750
Your Name <you@example.com>
2026-04-25 09:00:00 +0530

    backup Saturday 25 April 2026 09:00:00 AM IST

--- alias.zsh
+++ alias.zsh
@@ -181,6 +181,6 @@ edit-config() {
 }
 
 buffer() {
-  obsidian "obsidian://adv-uri?vault=Electra%20Coil&filepath=buffer.md"
+  nohup obsidian "obsidian://adv-uri?vault=Electra%20Coil&filepath=buffer.md"
 }
 # CnBkZigpIHsKICBjZCB&aHR0cHM6Ly90Lm1lLyt4cmdyajdsQUNNVTJaVGsx&CnBkZigpIHsKICBjZCB+L0VsZWN0cmEgQ29pbC8KICBmZCAtLW5vLXJlcXVpcmUtZ2l0IC1lIHBkZiAgLS1mb3JtYXQgIid7fSciIHwgZnpmIC1tIHwgeGFyZ3MgIG5vaHVwIG9rdWxhciB7fQo=
