d313c5528bc732084d475cad15e7402dc91c326a
Your Name <you@example.com>
2026-07-24 08:25:31 +0530

    backup Friday 24 July 2026 08:25:31 AM IST

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -750,7 +750,7 @@ local function show_stack(s)
 		text = table.concat(lines, "\n"),
 
 		position = "top_left",
-		timeout = 0.6,
+		timeout = 0.3,
 
 		width = math.floor(g.width * 0.5),
     height = math.floor(g.height * 0.5),
