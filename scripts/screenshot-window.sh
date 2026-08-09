7e35f5794573e393051c68796823414ccefd2fbb
Your Name <you@example.com>
2026-08-09 12:00:00 +0530

    backup Sunday 09 August 2026 12:00:00 PM IST

--- scripts/screenshot-window.sh
+++ scripts/screenshot-window.sh
@@ -1,2 +1,9 @@
 #!/bin/zsh
-flameshot screen -p ~/Pictures/"$(date +'%Y-%m-%d_%H-%M-%S')-fullscreen.png"
+# flameshot screen -p ~/Pictures/"$(date +'%Y-%m-%d_%H-%M-%S')-fullscreen.png"
+
+name="$(date +'%Y-%m-%d_%H-%M-%S')-fullscreen.png"
+xfce4-screenshooter -f -s ~/Pictures/"$name"
+xfce4-screenshooter -c -f &
+satty --filename ~/Pictures/"$name" --actions-on-enter save-to-clipboard --actions-on-escape save-to-file -o ~/Pictures/"$name"
+
+
