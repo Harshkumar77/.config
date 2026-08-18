82aec8198a8ed2f5fdd0af7ca7de7592697c743d
Your Name <you@example.com>
2026-08-18 21:00:00 +0530

    backup Tuesday 18 August 2026 09:00:00 PM IST

--- scripts/screenshot-open-latest.sh
+++ scripts/screenshot-open-latest.sh
@@ -1,4 +1,2 @@
 #!/bin/zsh
-
-xdg-open "$(ls -t ~/Pictures | head -n 1 | sed "s|^|$HOME/Pictures/|")"
-notify-send "screenshot $(date)"
+ xdg-open "$(ls -t ~/Pictures | head -n 1 | sed "s|^|$HOME/Pictures/|")"
