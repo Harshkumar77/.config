1a14908b734d23a3344cc6a9358b7d442c2a6acf
Your Name <you@example.com>
2026-08-19 14:03:55 +0530

    backup Wednesday 19 August 2026 02:03:55 PM IST

--- awesome/zenburn/theme.lua
+++ awesome/zenburn/theme.lua
@@ -25,8 +25,8 @@ theme.bg_systray = theme.bg_normal
 -- }}}
 
 -- {{{ Borders
-theme.useless_gap = dpi(0)
-theme.border_width = dpi(3)
+theme.useless_gap = dpi(2)
+theme.border_width = dpi(5)
 theme.border_normal = "#272822"
 theme.border_focus = "#c4265e"
 theme.border_marked = "#CC9393"
