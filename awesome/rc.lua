7bd072bfd1c79f3d7ee175f8362a9b5c111df79c
Your Name <you@example.com>
2026-09-06 19:16:38 +0530

    backup Sunday 06 September 2026 07:16:38 PM IST

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -709,11 +709,11 @@ awful.rules.rules = {
 
 	-- Set Firefox to always map on the tag named "2" on screen 1.
 	-- { rule = { class = "Firefox" }, properties = { screen = 1, tag = "fullscreen-II" } },
-	{ rule = { class = "firefox" }, properties = { screen = 1, tag = "todo" } },
-	{ rule = { class = "Google-chrome" }, properties = { screen = 1, tag = "todo" } },
+	-- { rule = { class = "firefox" }, properties = { screen = 1, tag = "todo" } },
+	-- { rule = { class = "Google-chrome" }, properties = { screen = 1, tag = "todo" } },
 	-- { rule = { class = "jetbrains-studio" }, properties = { screen = 1, tag = "fullscreen" } },
-	{ rule = { class = "mpv" }, properties = { screen = 1, tag = "todo" } },
-	{ rule = { class = "qBittorrent" }, properties = { screen = 1, tag = "todo" } },
+	-- { rule = { class = "mpv" }, properties = { screen = 1, tag = "todo" } },
+	-- { rule = { class = "qBittorrent" }, properties = { screen = 1, tag = "todo" } },
 	-- { rule = { class = "Thunar" }, properties = { screen = 1, tag = "main" } },
 	-- { rule = { class = "okular" }, properties = { screen = 1, tag = "main" } },
 	-- { rule = { class = "alacritty" }, properties = { screen = 1, tag = "2" } },
--- ts-scripts/key.ts
+++ ts-scripts/key.ts
@@ -6,7 +6,7 @@ const keys: Keys = {
   screenshotMode: "full",
   modTabMode: "alternative",
   singleTapMod: true,
-  titleBarEnabled: false,
+  titleBarEnabled: true,
   keyboardNative: true,
 }
 
